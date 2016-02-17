592494394417e42cdbc51ad37010877125269cea
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-02-17 17:17:24 +0100

    Added some shooting things

--- Solution/Entity/ShootingComponent.h
+++ Solution/Entity/ShootingComponent.h
@@ -7,7 +7,22 @@ public:
 	ShootingComponent(Entity& aEntity);
 	~ShootingComponent();
 
+	static eComponentType GetTypeStatic();
+	eComponentType GetType() override;
+
+	void ReceiveNote(const ShootNote& aNote) override;
+
 private:
 	
+	// ShootingComponentData
 };
 
+inline eComponentType ShootingComponent::GetTypeStatic()
+{
+	return eComponentType::SHOOTING;
+}
+
+inline eComponentType ShootingComponent::GetType()
+{
+	return GetTypeStatic();
+}
