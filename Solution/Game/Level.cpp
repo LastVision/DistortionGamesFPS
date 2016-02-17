b34708d564eb60306fee19497ed35591ed7ab0e5
LastVision <daniel.carlsson92@gmail.com>
2016-02-17 17:31:07 +0100

    Added Player Camera to Scene in LEvel

--- Solution/Game/Level.cpp
+++ Solution/Game/Level.cpp
@@ -10,7 +10,7 @@
 Level::Level()
 {
 	myPlayer = new PlayerComponent();
-	myScene = new Prism::Scene(*myPlayer->myCamera);
+	myScene = new Prism::Scene(*myPlayer->GetCamera());
 	myInstance = new Prism::Instance(*Prism::ModelLoader::GetInstance()->LoadModel("Data/Resource/Model/Modular_set/Dev_set/SM_dev_wall_corner_out_200_x_300.fbx", "Data/Resource/Shader/S_effect_pbl.fx")
 		, myInstanceOrientation, Prism::eOctreeType::DYNAMIC, 15.f);
 	myInstanceOrientation = CU::Matrix44<float>::CreateRotateAroundY(M_PI);
