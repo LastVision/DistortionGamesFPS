6b9fc9e3c0c9e001fd071e320e56977d467dbad0
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-02-24 09:55:30 +0100

    Empty class

---
+++ Solution/GUI/GUIManager3D.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "GUIManager3D.h"
+
+
+GUIManager3D::GUIManager3D()
+{
+}
+
+
+GUIManager3D::~GUIManager3D()
+{
+}
---
+++ Solution/GUI/GUIManager3D.h
@@ -0,0 +1,8 @@
+#pragma once
+class GUIManager3D
+{
+public:
+	GUIManager3D();
+	~GUIManager3D();
+};
+
