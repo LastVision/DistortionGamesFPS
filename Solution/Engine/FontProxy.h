454fa7cabcbd3b09f9f50e1daa6af50414bb7639
nian0601 <anderssonniklas@outlook.com>
2016-03-11 12:16:55 +0100

    Added TextProxy

--- Solution/Engine/FontProxy.h
+++ Solution/Engine/FontProxy.h
@@ -1,11 +1,28 @@
 #pragma once
 
+#include "CharacterData.h"
+#include <string.h>
+
 namespace Prism
-{
+{ 
+	class Font;
 	class FontProxy
 	{
 	public:
-		FontProxy();
+		FontProxy(const std::string& aFilePath);
 		~FontProxy();
+
+		Texture* GetTexture() const;
+		CharacterData GetCharData(unsigned char aChar) const;
+		const std::string& GetFilePath() const;
+
+		bool IsLoaded() const;
+		void SetFont(Font* aFont);
+
+	private:
+		void operator=(FontProxy&) = delete;
+
+		Font* myFont;
+		std::string myFilePath;
 	};
 }
---
+++ Solution/Engine/TextProxy.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "TextProxy.h"
+
+
+namespace Prism
+{
+	TextProxy::TextProxy()
+	{
+	}
+
+
+	TextProxy::~TextProxy()
+	{
+	}
+}
