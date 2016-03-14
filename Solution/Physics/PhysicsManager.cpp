dc0a52237a2a896adc4100521aadf4a078216549
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-03-14 17:54:07 +0100

    Might fixed something in physicsmanager

--- Solution/Physics/PhysicsManager.cpp
+++ Solution/Physics/PhysicsManager.cpp
@@ -711,9 +711,9 @@ namespace Prism
 		if (CU::FileExists(cowPath) == false)
 		{
 
-			if (!wfo.loadObj(cowPath.c_str(), false))
+			if (!wfo.loadObj(objPath.c_str(), false))
 			{
-				DL_ASSERT(CU::Concatenate("Error loading file: %s", cowPath.c_str()));
+				DL_ASSERT(CU::Concatenate("Error loading file: %s", objPath.c_str()));
 			}
 
 			physx::PxTriangleMeshDesc meshDesc;
