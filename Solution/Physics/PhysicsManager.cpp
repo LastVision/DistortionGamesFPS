6ac207c6dc3d5db620854f126f3b7551aecf78bd
namon <linus.ljungman@gmail.com>
2016-03-15 17:48:41 +0100

    removed visual debugger from Internal

--- Solution/Physics/PhysicsManager.cpp
+++ Solution/Physics/PhysicsManager.cpp
@@ -124,7 +124,7 @@ namespace Prism
 		myScene->setFlag(physx::PxSceneFlag::eENABLE_KINEMATIC_STATIC_PAIRS, true);
 		myScene->setSimulationEventCallback(this);
 
-#ifndef RELEASE_BUILD
+#ifdef _DEBUG
 		if (myPhysicsSDK->getPvdConnectionManager())
 		{
 			myPhysicsSDK->getPvdConnectionManager()->addHandler(*this);
