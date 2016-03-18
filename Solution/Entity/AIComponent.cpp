4c3f59cedb0ab68b54dd1242e87871913346ff5d
namon <linus.ljungman@gmail.com>
2016-03-18 15:59:35 +0100

    AI 0.01

--- Solution/Entity/AIComponent.cpp
+++ Solution/Entity/AIComponent.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "AIComponent.h"
-
+#include "PhysicsComponent.h"
+#include <PhysicsInterface.h>
 
 AIComponent::AIComponent(Entity& anEntity, const AIComponentData& aData)
 	: Component(anEntity)
@@ -16,5 +17,7 @@ AIComponent::~AIComponent()
 
 void AIComponent::Update(float aDelta)
 {
-
+	CU::Vector3<float> movement(0.f, 0.f, 1.f);
+	movement *= 0.13f;
+	Prism::PhysicsInterface::GetInstance()->Move(myEntity.GetComponent<PhysicsComponent>()->GetCapsuleControllerId(), movement, 0.05f, aDelta);
 }
