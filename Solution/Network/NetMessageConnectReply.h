0c0d1946ac2ab0a3c0fa8ebbf19ec1a9efb3debf
LastVision <daniel.carlsson92@gmail.com>
2016-03-18 13:16:09 +0100

    Fixed NetMessageConnectReply with inlined functions

--- Solution/Network/NetMessageConnectReply.h
+++ Solution/Network/NetMessageConnectReply.h
@@ -23,3 +23,34 @@ private:
 	void DoDeSerialize(StreamType& aStream) override;
 };
 
+inline NetMessageConnectReply::NetMessageConnectReply(eType aType, unsigned int aGID)
+	: NetImportantMessage(eNetMessageType::CONNECT_REPLY)
+	, myType(aType)
+	, myGID(aGID)
+{
+}
+
+inline NetMessageConnectReply::NetMessageConnectReply()
+	: NetImportantMessage(eNetMessageType::CONNECT_REPLY)
+{
+}
+
+inline NetMessageConnectReply::~NetMessageConnectReply()
+{
+}
+
+inline void NetMessageConnectReply::DoSerialize(StreamType& aStream)
+{
+	__super::DoSerialize(aStream);
+
+	SERIALIZE(aStream, myType);
+	SERIALIZE(aStream, myGID);
+}
+
+inline void NetMessageConnectReply::DoDeSerialize(StreamType& aStream)
+{
+	__super::DoDeSerialize(aStream);
+
+	DESERIALIZE(aStream, myType);
+	DESERIALIZE(aStream, myGID);
+}
