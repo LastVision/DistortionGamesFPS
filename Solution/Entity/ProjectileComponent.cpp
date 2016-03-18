469edb21d8c97101356be8e17a3dad7f28d32a10
namon <linus.ljungman@gmail.com>
2016-03-18 14:37:45 +0100

    added projectile component

---
+++ Solution/Entity/ProjectileComponent.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "ProjectileComponent.h"
+
+
+ProjectileComponent::ProjectileComponent()
+{
+}
+
+
+ProjectileComponent::~ProjectileComponent()
+{
+}
---
+++ Solution/Entity/ProjectileComponent.h
@@ -0,0 +1,8 @@
+#pragma once
+class ProjectileComponent
+{
+public:
+	ProjectileComponent();
+	~ProjectileComponent();
+};
+
