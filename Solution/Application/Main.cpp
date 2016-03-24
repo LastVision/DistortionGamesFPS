24e5834c070e333c1f98e77142d4458519ad698c
nian0601 <anderssonniklas@outlook.com>
2016-03-24 11:40:31 +0100

    Added commandline option to toggle SH-Generation

--- Solution/Application/Main.cpp
+++ Solution/Application/Main.cpp
@@ -8,6 +8,7 @@
 #include <TimerManager.h>
 #include <CommonHelper.h>
 #include <InputWrapper.h>
+#include <Defines.h>
 //#include <vld.h>
 
 bool engineIsRunning = true;
@@ -94,6 +95,10 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPTSTR, int aNumberCommands)
 			else if (command == "-useEntityLog")
 			{
 				DL_Debug::Debug::GetInstance()->ActivateFilterLog(DL_Debug::eFilterLog::ENTITY);
+			}
+			else if (command == "-generateLightData")
+			{
+				Global_GenerateLightData = true;
 			}
 		}
 
