e27b105babaf554c32e06b486e9b03c1d7241241
LastVision <daniel.carlsson92@gmail.com>
2016-03-31 16:38:36 +0200

    Fixed so you can spawn units by using missionevent

--- Solution/Game_Server/DefendMission.cpp
+++ Solution/Game_Server/DefendMission.cpp
@@ -36,6 +36,26 @@ bool DefendMission::Update(float aDeltaTime)
 		{
 			myDefendTime -= aDeltaTime;
 		}
+		if (myMissionEvents.Size() > 0)
+		{
+			myMissionEvents[myCurrentMissionEvent].myTimeBeforeStarting -= aDeltaTime;
+			if (myMissionEvents[myCurrentMissionEvent].myTimeBeforeStarting <= 0.f)
+			{
+				SendMissionMessage(myMissionEvents[myCurrentMissionEvent]);
+				++myCurrentMissionEvent;
+				if (myCurrentMissionEvent == myMissionEvents.Size())
+				{
+					if (myShouldLoopMissionEvents == true)
+					{
+						myCurrentMissionEvent = 0;
+					}
+					else
+					{
+						myMissionEvents.RemoveAll();
+					}
+				}
+			}
+		}
 		//test:
 		//if (myLastSecondToWarn == int(myDefendTime))
 		//{
