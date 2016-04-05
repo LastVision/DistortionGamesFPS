4837a04b91c9625ae314d0e553c870e11fc24a51
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-04-05 15:28:24 +0200

    Trigger doesn't add itself if client side on serverty

--- Solution/Entity/PhysicsComponent.cpp
+++ Solution/Entity/PhysicsComponent.cpp
@@ -3,6 +3,7 @@
 #include "PhysicsComponent.h"
 #include "PhysicsComponentData.h"
 #include <PhysicsInterface.h>
+#include "TriggerComponent.h"
 #include <CommonHelper.h>
 
 
@@ -24,12 +25,26 @@ PhysicsComponent::PhysicsComponent(Entity& aEntity, const PhysicsComponentData& 
 
 	myPhysicsType = aPhysicsComponentData.myPhysicsType;
 
+	if (myEntity.GetGID() == 502)
+	{
+		int apa = 5;
+		apa;
+	}
+
 	bool shouldAddToPhysicsScene = true;
 	if (myEntity.GetType() == eEntityType::EXPLOSION || myEntity.GetSubType() == "respawn"
 		|| myEntity.GetType() == eEntityType::BULLET || myEntity.GetSubType() == CU::ToLower("gunDroidServer")
 		|| myEntity.GetSubType() == "gundroid")
 	{
 		shouldAddToPhysicsScene = false;
+	}
+
+	if (myEntity.GetIsClient() == false && myEntity.GetComponent<TriggerComponent>() != nullptr)
+	{
+		if (myEntity.GetComponent<TriggerComponent>()->IsClientSide() == true)
+		{
+			shouldAddToPhysicsScene = false;
+		}
 	}
 
 	if (myPhysicsType != ePhysics::CAPSULE)
