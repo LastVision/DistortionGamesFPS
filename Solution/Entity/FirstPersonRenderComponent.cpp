aadf19118cb13a6d4da3bb5420eaa317b1995ff6
apasitteribuske <malin.sandgren.sp14@thegameassembly.com>
2016-04-14 12:09:36 +0200

    Respawn

--- Solution/Entity/FirstPersonRenderComponent.cpp
+++ Solution/Entity/FirstPersonRenderComponent.cpp
@@ -456,10 +456,10 @@ void FirstPersonRenderComponent::Render(Prism::Texture* aArmDepthTexture, bool a
 
 			float color = myCoOpCircles[i].myLifePercentage;
 
-			if (myCoOpRespawns.Size() > 0)
+			if (myCoOpCircles[i].myLifePercentage <= 0.f)
 			{
-				myCoOpSprite->Render({ newRenderPos.x, newRenderPos.y - 1.f }, { 1.f, 1.f }, { 1.f - color, color, color, 1.f });
-				Prism::Engine::GetInstance()->PrintText(myCoOpRespawns[i].myCurrentValue, { newRenderPos.x, newRenderPos.y - 1.f }, Prism::eTextType::RELEASE_TEXT);
+				myCoOpSprite->Render({ newRenderPos.x, newRenderPos.y - 0.5f }, { 1.f, 1.f }, { 1.f - color, color, color, 1.f });
+				Prism::Engine::GetInstance()->PrintText(myCoOpRespawns[i].myCurrentValue, { newRenderPos.x, newRenderPos.y - 0.5f }, Prism::eTextType::RELEASE_TEXT);
 			}
 			else
 			{
